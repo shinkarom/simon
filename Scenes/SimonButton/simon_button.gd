85151f154b3d565ffcdce0784489a69231f889b9
shinkarom <shinkarom@gmail.com>
2023-04-19 12:21:45 +0300

    Add buttons to the main scene

--- Scenes/SimonButton/simon_button.gd
+++ Scenes/SimonButton/simon_button.gd
@@ -5,13 +5,18 @@ var simpletexture = load("res://Assets/"+colorString+".png")
 var pressedtexture = load("res://Assets/"+colorString+"_pressed.png")
 
 @export var color: String:
-	get:
-		return colorString
+	get:	return colorString
 	set(value):
 		colorString = value
+		simpletexture = load("res://Assets/"+colorString+".png")
+		pressedtexture = load("res://Assets/"+colorString+"_pressed.png")
 		$TextureButton.texture_normal = simpletexture
 		$TextureButton.texture_hover = simpletexture
-		$TextureButton.texture_pressed = pressedtexture
+		$TextureButton.texture_pressed = pressedtexture	
+		if pressed:
+			$TextureButton.texture_disabled = pressedtexture
+		else:
+			$TextureButton.texture_disabled = simpletexture
 		
 var ispressed = false
 @export var pressed: bool:
@@ -41,7 +46,7 @@ func activate():
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
